c0fcc62d18f4c42598f5028a457b3347b7fcd03d
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-02-05 13:04:51 -0500

    2D arrays finished

---
+++ src/main/java/TwoDArrayDS.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+
+public class TwoDArrayDS {
+
+    /*
+    Given a 6x6 2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for
+every hourglass in , then print the maximum hourglass sum.
+
+Function Description
+
+Complete the function hourglassSum in the editor below. It should return an integer, the maximum hourglass sum in the
+array.
+
+hourglassSum has the following parameter(s):
+
+arr: an array of integers
+    */
+
+    public Integer findTheHourGlassSums(Integer[][] sixBySixArray) throws IOException {
+        Integer largestSum = -63;
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 4; j++){
+                if(sixBySixArray[j][i] > 9 || sixBySixArray[j][i] < -9){
+                    throw new IOException("Please enter an integer between -9 and 9");
+                }
+                Integer currentSum = 0;
+                currentSum += sixBySixArray[j][i] +
+                        sixBySixArray[j - 1][i - 1] + sixBySixArray[j - 1][i] + sixBySixArray[j - 1][i + 1] +
+                        sixBySixArray[j + 1][i - 1] + sixBySixArray[j + 1][i] + sixBySixArray[j + 1][i + 1];
+                if(currentSum > largestSum){
+                    largestSum = currentSum;
+                }
+            }
+        }
+        return largestSum;
+    }
+}
---
+++ src/test/java/TwoDArrayDSTest.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class TwoDArrayDSTest {
+    TwoDArrayDS twoDArrayDS = new TwoDArrayDS();
+
+    @Test
+    public void provided_example() throws IOException {
+        //Given
+        Integer[][] example = {{1,1,1,0,0,0},
+                               {0,1,0,0,0,0},
+                               {1,1,1,0,0,0},
+                               {0,0,2,4,4,0},
+                               {0,0,0,2,0,0},
+                               {0,0,1,2,4,0}};
+        //When
+        Integer expected = 19;
+        Integer actual = twoDArrayDS.findTheHourGlassSums(example);
+        //Then
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void larger_array() throws IOException {
+        //Given
+        Integer[][] example = {{-9,-9,-9,9,9,9},
+                {-9,-3,6,7,-4,0},
+                {2,3,5,7,0,1},
+                {5,9,6,4,4,1},
+                {-2,-4,-9,2,5,3},
+                {-1,-4,4,2,4,-8}};
+        //When
+        Integer expected = 34;
+        Integer actual = twoDArrayDS.findTheHourGlassSums(example);
+        //Then
+        assertEquals(expected,actual);
+    }
+
+    @Test (expected = IOException.class)
+    public void number_out_of_range() throws IOException {
+        //Given
+        Integer[][] example = {{-9,-9,-9,9,9,9},
+                {-9,-3,6,7,-4,0},
+                {2,3,5,7,0,1},
+                {5,9,6,-10,4,1},
+                {-2,-4,-9,2,5,3},
+                {-1,-4,4,2,4,-8}};
+        //When
+        Integer expected = 34;
+        Integer actual = twoDArrayDS.findTheHourGlassSums(example);
+        //Then
+    }
+}
