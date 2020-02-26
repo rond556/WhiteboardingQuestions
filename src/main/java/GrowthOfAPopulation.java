431750ad729dbc54b664347c900d43022136f114
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-02-26 14:08:19 -0500

    Growth of a Population

---
+++ src/main/java/GrowthOfAPopulation.java
@@ -0,0 +1,29 @@
+public class GrowthOfAPopulation {
+    /*In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+At the end of the first year there will be:
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be:
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (number of inhabitants is an integer)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+More generally given parameters:
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.*/
+
+    public static int findThePopulation(int population, double percent,
+                                        int additionalPeople, int finalPopulation){
+        int numberOfYears = 0;
+        while(population < finalPopulation){
+            population += population * (percent / 100) + additionalPeople;
+            numberOfYears++;
+        }
+        return numberOfYears;
+    }
+}
